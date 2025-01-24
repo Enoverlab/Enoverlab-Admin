37aefe0665d33f2a7de654a4cfe4a033b4b14b83
oladee <deemajor230600@gmail.com>
2025-01-24 23:30:54 +0100

    minor mix

--- src/admin/options.ts
+++ src/admin/options.ts
@@ -1,8 +1,8 @@
 import { AdminJSOptions } from 'adminjs';
-import { coursesResource,moduleResource } from 'src/courses/courses.resource.js';
+import { coursesResource,moduleResource } from '../courses/courses.resource.js';
 
 import componentLoader from './component-loader.js';
-import { authorResource, blogResource } from 'src/blog/blog.resources.js';
+import { authorResource, blogResource } from '../blog/blog.resources.js';
 
 
 
